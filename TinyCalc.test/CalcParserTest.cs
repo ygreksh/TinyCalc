785dccd3ef44c72e4afc21574a4ad63191ca2b77
ygrek <ygreksh@gmail.com>
2021-08-24 16:58:40 +0300

    some unit tests

---
+++ TinyCalc.test/CalcParserTest.cs
@@ -0,0 +1,68 @@
+﻿using Xunit;
+
+namespace TinyCalc.test
+{
+    public class CalcParserTest
+    {
+        [Fact]
+        public void Sum_2_Plus_2_Eq_4()
+        {
+            //Arrange
+            int a = 2;
+            int b = 2;
+            //Act
+            int result = CalcParser.Sum(a, b);
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(4,result);
+        }
+        [Fact]
+        public void Sum_2_Plus_0_Eq_2()
+        {
+            //Arrange
+            int a = 2;
+            int b = 0;
+            //Act
+            int result = CalcParser.Sum(a, b);
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(2,result);
+        }
+        [Fact]
+        public void Substr_2_minus_2_Eq_0()
+        {
+            //Arrange
+            int a = 2;
+            int b = 2;
+            //Act
+            int result = CalcParser.Substr(a,b);
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(0,result);
+        }
+        [Fact]
+        public void Substr_5_minus_10_Eq_min5()
+        {
+            //Arrange
+            int a = 5;
+            int b = 10;
+            //Act
+            int result = CalcParser.Substr(a,b);
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(-5,result);
+        }
+        [Fact]
+        public void Multi_3_mult_4_Eq_12()
+        {
+            //Arrange
+            int a = 3;
+            int b = 4;
+            //Act
+            int result = CalcParser.Multi(a,b);
+            //Assert
+            Assert.NotNull(result);
+            Assert.Equal(12,result);
+        }
+    }
+}
---
+++ TinyCalc.test/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TinyCalc.test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
