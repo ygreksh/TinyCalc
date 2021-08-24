10e462a76267c84481d99e01f2771f24bea670ea
ygrek <ygreksh@gmail.com>
2021-08-24 13:40:49 +0300

    1

---
+++ TinyCalc/CalcParser.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Linq;
+
+namespace TinyCalc
+{
+    public static class CalcParser
+    {
+        public static int Calculate(string input)
+        {
+            int result;
+            char[] opSigns = new[] { '+', '-', '*', '/' };
+            string[] args = input.Split(opSigns);
+            int a = Int32.Parse(args[0]);
+            int b = Int32.Parse(args[1]);
+            if (input.Contains('+'))
+            {
+                result = a + b;
+            }
+            else if (input.Contains('-'))
+            {
+                result = a - b;
+            }
+            else if (input.Contains('*'))
+            {
+                result = a * b;
+            }else if (input.Contains('/'))
+            {
+                result = a / b;
+            }
+            else
+            {
+                result = 0;
+                Console.WriteLine("Неверный сивол операции");
+            }
+            return result;
+        }
+
+        public static int Sum(int a, int b)
+        {
+            return a + b;
+        }
+        public static int Substr(int a, int b)
+        {
+            return a - b;
+        }
+        public static int Multi(int a, int b)
+        {
+            return a * b;
+        }
+        public static int Divide(int a, int b)
+        {
+            int result;
+            try
+            {
+                result = a / b;
+                return result;
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine("На 0 делить нельзя");
+                throw;
+            }
+        }
+    }
+}
---
+++ TinyCalc/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace TinyCalc
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите выражение:");
+            string inputString = Console.ReadLine();
+            int result = CalcParser.Calculate(inputString);
+            Console.Write(result);
+        }
+    }
+}
